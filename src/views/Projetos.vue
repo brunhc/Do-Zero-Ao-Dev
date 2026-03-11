93c41a314c531024569b1d74f4df844b6ca2b7e7
brunhc <brunhc@gmail.com>
2026-03-10 23:53:39 -0300

    Auto-commit: src/views/Projetos.vue at 3/10/2026, 11:53:39 PM

--- src/views/Projetos.vue
+++ src/views/Projetos.vue
@@ -1,37 +1,41 @@
 <template>
-    <div>
-      <h1>Meus Projetos</h1>
-      <p>Esta é a página onde listarei meus projetos.</p>
-      <ul>
-        <li><a href="/conversor-precos">Conversor</a></li>
-        <li><a href="/calculadora-rpa">Calculadora RPA Bidirecional</a></li>
-        <li><a href="#">Projeto 3</a></li>
-      </ul>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'ProjectsPage',
-  };
-  </script>
-  
-  <style scoped>
-  h1 {
-    color: #2c3e50;
-  }
-  ul {
-    list-style-type: none;
-    padding: 0;
-  }
-  li {
-    margin: 10px 0;
-  }
-  a {
-    color: #42b983;
-    text-decoration: none;
-  }
-  a:hover {
-    text-decoration: underline;
-  }
-  </style>
+  <div>
+    <h1>Meus Projetos</h1>
+    <p>Esta é a página onde listarei meus projetos.</p>
+    <ul>
+      <li><router-link to="/conversor-precos">Conversor</router-link></li>
+      <li><router-link to="/calculadora-rpa">Calculadora RPA Bidirecional</router-link></li>
+      <li><a href="#">Projeto 3</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProjectsPage',
+};
+</script>
+
+<style scoped>
+h1 {
+  color: #2c3e50;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  margin: 10px 0;
+}
+
+a {
+  color: #42b983;
+  text-decoration: none;
+}
+
+a:hover {
+  text-decoration: underline;
+}
+</style>
