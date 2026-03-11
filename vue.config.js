23b139dabb28fb857b8a77ba24e820a68d6594ab
brunhc <brunhc@gmail.com>
2026-03-10 23:39:13 -0300

    Auto-commit: .vscode/settings.json, vue.config.js at 3/10/2026, 11:39:13 PM

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,5 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   lintOnSave: false,
   transpileDependencies: true,
-  publicPath: '/'
-
+  publicPath: process.env.NODE_ENV === 'production' ? '/Do-Zero-Ao-Dev/' : '/'
 })
